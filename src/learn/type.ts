fdfd48a9b53ab73340b5586efc9bef93c4fb8acb
zhx <your.email@example.com>
2025-07-30 13:50:33 +0800

    fir

---
+++ src/learn/type.ts
@@ -0,0 +1,22 @@
+const i: boolean = false
+function x(): void {}
+let xx: string | number
+interface Person {
+  name: string
+  [propName: string]: any
+}
+const person: Person = {
+  name: 'z',
+}
+
+const arr: Array<Person> = [person]
+
+interface SF {
+  (x: string): boolean
+}
+const f: SF = (x) => {
+  console.log(x)
+  return true
+}
+
+//断言
---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import TheWelcome from '../components/TheWelcome.vue'
+
+interface F {
+  (x: number): void
+}
+type F2 = InstanceType<typeof TheWelcome>
+const i = ref(1)
+const ref1 = ref<F2>()
+console.log(ref1.value?.x1)
+onMounted(() => {
+  console.log(ref1.value?.x1)
+})
+const c: F = (x: number): void => {
+  i.value = 3
+  console.log(i.value)
+}
+defineExpose({ x: i })
+</script>
+
+<template>
+  <main>
+    <button @click="c(1)">xxxx</button>
+    {{ i }}
+    <TheWelcome :i ref="ref1" />
+  </main>
+</template>
