2ba6a5c19138068eff3eb37e5a839c385fabd60f
zhx <your.email@example.com>
2025-07-30 13:57:09 +0800

    ++

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,14 @@ const click = () => {
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
     <div style="color: red">{{ a }}</div>
     <button @click="click">x</button>
-    <!-- <div class="wrapper">
+    <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div> -->
+    </div>
   </header>
 
   <RouterView />
